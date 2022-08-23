14045581c2ab4f596f962d68c3c0e9a2b9c10286
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-08-23 15:13:50 -0400

    Add and test responsiveness for many different screen sizes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,19 @@
-// Make sure to add Socials (icon links)
-
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import './NavBar.css'
 
 export default function NavBar() {
     return (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Merch</Link>
-            <Link to='/'>Shows</Link>
-            <Link to='/'>Song List</Link>
-            <Link to='/'>Video</Link>
-        </nav>
+        <main>
+            {/* <img src="public/120145231_1742117679296647_8151332947136239940_n.jpg" alt="band-pic" /> */}
+            <nav className="navbar container">
+                <Link to='/' className="child">HOME</Link>
+                <Link to='/' className="child">CONTACT</Link>
+                <Link to='/' className="child">MERCH</Link>
+                <Link to='/' className="child">SHOWS</Link>
+                <Link to='/' className="child">SONG LIST</Link>
+                <Link to='/' className="child">VIDEO</Link>
+                {/* // Make sure to add Socials (icon links) */}
+            </nav>
+        </main>
     )
 }
