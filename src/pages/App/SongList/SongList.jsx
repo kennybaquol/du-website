605d3f3a41f7dc00508aa374dc8d82514aa158b5
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-08-23 16:53:57 -0400

    Separate concerns regarding different pages' styling

--- src/pages/App/SongList/SongList.jsx
+++ src/pages/App/SongList/SongList.jsx
@@ -3,132 +3,132 @@ import './SongList.css'
 export default function SongList() {
     return (
         <main>
-            <h1>Song List:</h1>
-            <h2>
+            <h1 className='song-list-header'>Song List:</h1>
+            <h2 className="song-list">
                 <div className='item'>
-                    Alice In Chains - Them Bones<br />
+                    Alice In Chains - Them Bones
                 </div>
                 <div className='item'>
-                    Alien Ant Farm - Movies<br />
+                    Alien Ant Farm - Movies
                 </div>
                 <div className='item'>
-                    Audioslave - Like A Stone<br />
+                    Audioslave - Like A Stone
                 </div>
                 <div className='item'>
-                    Avenged Sevenfold - Nightmare<br />
+                    Avenged Sevenfold - Nightmare
                 </div>
                 <div className='item'>
                     Buckcherry - Crazy Bitch,
-                    Lit Up<br />
+                    Lit Up
                 </div>
                 <div className='item'>
                     Bush - Comedown,
-                    Machine Head<br />
+                    Machine Head
                 </div>
                 <div className='item'>
-                    The Calling - Wherever You Will Go<br />
+                    The Calling - Wherever You Will Go
                 </div>
                 <div className='item'>
-                    Candlebox - Far Behind<br />
+                    Candlebox - Far Behind
                 </div>
                 <div className='item'>
-                    Collective Soul - Heavy<br />
+                    Collective Soul - Heavy
                 </div>
                 <div className='item'>
-                    Creed - Higher<br />
+                    Creed - Higher
                 </div>
                 <div className='item'>
-                    Creedence Clearwater Revival - Medley of Hits<br />
+                    Creedence Clearwater Revival - Medley of Hits
                 </div>
                 <div className='item'>
-                    Eve 6 - Inside Out<br />
+                    Eve 6 - Inside Out
                 </div>
                 <div className='item'>
-                    Fall Out Boy - Sugar, We’re Going Down<br />
+                    Fall Out Boy - Sugar, We’re Going Down
                 </div>
                 <div className='item'>
-                    Finger Eleven - Paralyzer<br />
+                    Finger Eleven - Paralyzer
                 </div>
                 <div className='item'>
                     Foo Fighters - Everlong,
                     Learn To Fly,
-                    My Hero<br />
+                    My Hero
                 </div>
                 <div className='item'>
                     Fuel - Bittersweet,
-                    Hemorrhage<br />
+                    Hemorrhage
                 </div>
                 <div className='item'>
-                    Goo Goo Dolls - Slide<br />
+                    Goo Goo Dolls - Slide
                 </div>
                 <div className='item'>
-                    Guns N’ Roses - Sweet Child O’ Mine<br />
+                    Guns N’ Roses - Sweet Child O’ Mine
                 </div>
                 <div className='item'>
-                    Highly Suspect - My Name Is Human<br />
+                    Highly Suspect - My Name Is Human
                 </div>
                 <div className='item'>
-                    Lit - My Own Worst Enemy<br />
+                    Lit - My Own Worst Enemy
                 </div>
                 <div className='item'>
                     Lynyrd Skynyrd - Free Bird,
-                    Simple Man<br />
+                    Simple Man
                 </div>
                 <div className='item'>
                     Metallica - Enter Sandman,
                     For Whom The Bell Tolls,
-                    Wherever I May Roam<br />
+                    Wherever I May Roam
                 </div>
                 <div className='item'>
-                    Nirvana - Come As You Are<br />
+                    Nirvana - Come As You Are
                 </div>
                 <div className='item'>
-                    Pearl Jam - Alive<br />
+                    Pearl Jam - Alive
                 </div>
                 <div className='item'>
-                    Poison - Nothin’ But A Good Time<br />
+                    Poison - Nothin’ But A Good Time
                 </div>
                 <div className='item'>
                     Rage Against The Machine - Bulls On Parade,
-                    Killing In The Name<br />
+                    Killing In The Name
                 </div>
                 <div className='item'>
                     Red Hot Chili Peppers - By The Way,
-                    Otherside<br />
+                    Otherside
                 </div>
                 <div className='item'>
-                    The Rolling Stones - Jumpin’ Jack Flash<br />
+                    The Rolling Stones - Jumpin’ Jack Flash
                 </div>
                 <div className='item'>
-                    Seven Mary Three - Cumbersome<br />
+                    Seven Mary Three - Cumbersome
                 </div>
                 <div className='item'>
-                    Steppenwolf - Born To Be Wild<br />
+                    Steppenwolf - Born To Be Wild
                 </div>
                 <div className='item'>
                     Stone Temple Pilots - Big Empty,
                     Interstate Love Song,
                     Plush,
-                    Wicked Garden<br />
+                    Wicked Garden
                 </div>
                 <div className='item'>
-                    Third Eye Blind - Jumper<br />
+                    Third Eye Blind - Jumper
                 </div>
                 <div className='item'>
-                    The Toadies - Possum Kingdom<br />
+                    The Toadies - Possum Kingdom
                 </div>
                 <div className='item'>
-                    Tool - Sober<br />
+                    Tool - Sober
                 </div>
                 <div className='item'>
                     Van Halen - Eruption,
-                    You Really Got Me<br />
+                    You Really Got Me
                 </div>
                 <div className='item'>
-                    Velvet Revolver - Slither<br />
+                    Velvet Revolver - Slither
                 </div>
                 <div className='item'>
-                    3 Doors Down - Kryptonite<br />
+                    3 Doors Down - Kryptonite
                 </div>
             </h2>
             <br />
