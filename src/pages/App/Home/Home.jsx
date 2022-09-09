a523495c35313c10f5b52a4fec9637c35b3ca805
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-09-09 13:49:13 -0400

    Update home page bio

--- src/pages/App/Home/Home.jsx
+++ src/pages/App/Home/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
     return (
         <main>
             <div className='home-page'>
-                <p className='home-child'>As the music world constantly changes, Downhill Uprise aggressively strives to be the high-energy, spirited band that rock music longs to keep alive. Formed in 2019, 4 passionate musicians strive tirelessly to prove that rock and its soul are alive and well. Their songs are often met with powerful and gritty vocals, ripping guitar licks, rumbling bass, and thundering, yet precise, drumming. Influenced by bands like Metallica, Buckcherry, Avenged Sevenfold, and Breaking Benjamin, these 4 Sovereigns of String are on a mission to contribute to a new chapter in rock history.</p>
+                <p className='home-child'>If you're looking for a great 90's and 00's Rock experience, then you've found it! Downhill Uprise will bring the best high energy show to any venue or event. Their takes on your favorite classics are often met with powerful and gritty vocals, ripping guitar licks, rumbling bass, and precise thundering of drums. This will bring you back to the best days of Stone Temple Pilots, Metallica, Pearl Jam, and so much more! With great setlists that will keep you on your feet and singing all night! Join us when we host an amazing night of your favorite rock songs.
+</p>
                 <img className='home-child' src={home} alt="Home" />
             </div>
 
